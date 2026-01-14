88f79f32748b80f519cd6f65b49039b735ec929c
Souvik Biswas <biswassouvik284@gmail.com>
2026-01-14 12:43:42 +0530

    fix: cover if the input leave request body is null

--- src/utils/lambda.utils.ts
+++ src/utils/lambda.utils.ts
@@ -6,13 +6,13 @@ export function createResponse(body: string, statusCode: number = 200) {
 }
 
 export function isValidLeaveRequest(obj: any): boolean {
-  return (
+  return Boolean(
     obj &&
-    typeof obj === 'object' &&
-    Object.keys(obj).length > 0 &&
-    typeof obj.email === 'string' &&
-    typeof obj.fromDate === 'string' &&
-    typeof obj.toDate === 'string' &&
-    typeof obj.reason === 'string'
+      typeof obj === 'object' &&
+      Object.keys(obj).length > 0 &&
+      typeof obj.email === 'string' &&
+      typeof obj.fromDate === 'string' &&
+      typeof obj.toDate === 'string' &&
+      typeof obj.reason === 'string'
   );
 }
